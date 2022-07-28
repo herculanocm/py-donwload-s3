14b7817e50fd0f464cce257f7a28fd8187951721
Herculano Cunha <herculano.cunha2@gmail.com>
2022-07-28 14:08:12 -0300

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,21 +1,21 @@
 # Desenvolvido por Herculano Cunha - Area de BI - (34) 3218 - 1452 / (34) 9 9794 - 6294
 
 # Config Log
-file_path_log =  'Sistemas/Tribanco/TransacoesPF/Log/' #Nao alterar
+file_path_log =  'Sistemas/Tresbanco/TransacoesPF/Log/' #Nao alterar
 file_name_log = 'transacoesPfBatch' #Nao alterar
 # =========================================================================
 #Config Amazon S3
 bucket_name = 'hml-pd-trb-quicksight'
-# bucket_name = 'dsv-bi-tribanco'
+# bucket_name = 'dsv-bi-tresbanco'
 file_ext_name = '.csv'  #Nao alterar
-prefix_name='exports-bi-tribanco/'
+prefix_name='exports-bi-tresbanco/'
 # credenciais
 aws_access_key_id=''
 aws_secret_access_key=''
 # credencias de dsv
 # aws_access_key_id='AKIAJXLV6FNK4IX4USSA'
 # aws_secret_access_key='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'
-file_path_downloads = 'Sistemas/Tribanco/TransacoesPF/'  #Nao alterar
+file_path_downloads = 'Sistemas/Tresbanco/TransacoesPF/'  #Nao alterar
 # Usado para captura de data
 file_name_ini = 'transacions-' #Nao alterar
 # =========================================================================
